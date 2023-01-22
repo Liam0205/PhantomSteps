e57366ded5eb87807d31aa2bb708e7474a989e91
Liam Huang <liamhuang0205@gmail.com>
2023-01-22 11:32:36 +0800

    [psp] dismiss keyboard correctly.

--- phantomstepspreferences/pspRootListController.m
+++ phantomstepspreferences/pspRootListController.m
@@ -11,6 +11,45 @@
   return _specifiers;
 }
 
+// ----- dismiss keyboard -----
+// - dissmiss on scroll
+- (void)loadView {
+  [super loadView];
+  ((UITableView *)[self table]).keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
+}
+
+// - dismiss on tab outside text field
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+
+  [self.view addGestureRecognizer:tap];
+}
+
+-(void)dismissKeyboard {
+  [self.view endEditing:YES];
+}
+
+// - dismiss on press return key
+-(void)_returnKeyPressed:(id)arg1 {
+  [self.view endEditing:YES];
+}
+
+// ---
+
+- (void)generateStepsATOnce {
+  UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"成功 / Success"
+                                message:@"假的啦！还没实现功能呢！"
+                                preferredStyle:UIAlertControllerStyleAlert];
+
+  UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+    handler:^(UIAlertAction * action) {}];
+
+  [alert addAction:defaultAction];
+  [self presentViewController:alert animated:YES completion:nil];
+}
+
 - (void)openBlogZH {
   NSDictionary *URLOptions = @{UIApplicationOpenURLOptionUniversalLinksOnly : @FALSE};
   [[UIApplication sharedApplication] openURL: [NSURL URLWithString:@"https://liam.page/"] options: URLOptions completionHandler: nil];
